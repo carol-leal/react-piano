25b2b65469b014aa421edb87e2b87fc27f71579d
Carol <carolinalealrequena@gmail.com>
2025-07-25 12:46:45 +0200

    key pressing

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -2,18 +2,43 @@ import { FunctionComponent } from "react";
 import clsx from "clsx";
 import { NoteType } from "../../domain/note";
 import styles from "./Key.module.css";
+import { usePressObserver } from "../PressObserver";
+
+type PressCallback = () => void;
 
 type KeyProps = {
   type: NoteType;
   label: string;
   disabled?: boolean;
+  onUp: PressCallback;
+  onDown: PressCallback;
 };
 
-export const Key: FunctionComponent<KeyProps> = (props) => {
-  const { type, label, ...rest } = props;
+export const Key: FunctionComponent<KeyProps> = ({
+  type,
+  label,
+  onDown,
+  onUp,
+  ...rest
+}) => {
+  const pressed = usePressObserver({
+    watchKey: label,
+    onStartPress: onDown,
+    onFinishPress: onUp,
+  });
 
   return (
-    <button className={clsx(styles.key, styles[type])} type="button" {...rest}>
+    <button
+      className={clsx(
+        styles.key,
+        styles[type],
+        pressed && styles["is-pressed"]
+      )}
+      type="button"
+      onMouseDown={onDown}
+      onMouseUp={onUp}
+      {...rest}
+    >
       {label}
     </button>
   );
